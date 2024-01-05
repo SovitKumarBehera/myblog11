fc8342bd2267a42f662dfbc7ab44808f3a4fb30e
Sovit <sovitsk1998@gmail.com>
2024-01-05 18:26:37 +0530

    127:Create save Post Feature Entity Class added

---
+++ src/main/java/com/myblog/myblog11/entity/Post.java
@@ -0,0 +1,21 @@
+package com.myblog.myblog11.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name="posts")//in the database i want the table name as post
+@Entity
+@Data//because i have added lombok in project structure,so using this automatically getters and setters are
+    //created for this below variables
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {          //dt-03/01/2024
+    @Id//for primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//for auto increment
+    private long id;
+    private String title;
+    private String description;
+    private String content;
+}
